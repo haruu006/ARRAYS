28ee45fd7c211e5096facaff155722de2c18e3b4
haruu006 <133334074+haruu006@users.noreply.github.com>
2023-08-06 20:38:22 +0530

    Update 26-Remove Duplicates from Sorted Array.java

--- 26-Remove Duplicates from Sorted Array.java
+++ 26-Remove Duplicates from Sorted Array.java
@@ -20,11 +20,11 @@ class Solution {
         int i=1;
         for(int j=1;j<nums.length;j++){
             if(nums[j]!=nums[j-1]){
-                nums[j]=nums[i];
+                nums[i]=nums[j];
                 i++;
             }
         }
         return i;
 
     }
-}  
+}
